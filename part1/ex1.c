74a0d421e683b1b6c1c770459ec856331f5ced19
tathagat <tathagat.thapliyal@gmail.com>
2018-02-28 23:29:07 +0530

    MAKEFILE MADE

---
+++ part1/ex1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(int args,char *argv[]){
+	int distance=100;
+	printf("you are %d miles away from me. ",distance);
+	return 0;
+}
---
+++ part2/ex3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int args,char * argv[]){
+	int age=12;
+	int height=160;
+
+	printf("i am %d years old.",age);
+	printf("i am %d inches tall.",height);
+	return 0;
+}
